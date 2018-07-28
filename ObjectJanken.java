45f22e039e73a79010acfbb1b87fc388fcb4501c
kazu_wsx <kazu.wsx@gmail.com>
2018-07-28 15:50:40 +0900

    戦術を生成してプレイヤーに渡す

--- ObjectJanken.java
+++ ObjectJanken.java
@@ -1,20 +1,33 @@
 /**
 *オブジェクト指向によるジャンケンプログラム
+*戦術インタフェースに対応
 */
 public class ObjectJanken
 {
     public static void main(String[] args)
     {
-        //審判（斎藤さん）のインスタンスの生成
-        Judge saito = new Judge();
+        //審判のインスタンスの生成
+        Judge judge = new Judge();
 
         //プレイヤー１（村田さん）の生成
-        Player murata = new Murata("村田さん");
+        Player murata = new Player("村田さん");
+
+        //村田さんに渡す戦略クラスを生成する
+        Tactics murataTactics = new RandomTactics();
+
+        //村田さんに戦略クラスを渡す
+        murata.setTactics(murataTactics);
 
         //プレイヤー２（山田さん）の生成
-        Player yamada = new Yamada("山田さん");
+        Player yamada = new Player("山田さん");
+
+        //山田さんに渡す戦略クラスを生成
+        Tactics yamadaTactics = new RandomTactics();
+
+        //山田さんに戦術クラスを渡す
+        yamada.setTactics(yamadaTactics);
 
         //村田さんと山田さんをプレイヤーとしてジャンケンを開始する
-        saito.startJanken(murata,yamada);
+        judge.startJanken(murata,yamada);
     }
 }
